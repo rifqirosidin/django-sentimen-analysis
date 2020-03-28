9ad80fccae47127514b993702692c33bf8b4540f
Rifqi Rosidin <rifqi.rosidin@gmail.com>
2020-03-28 19:45:48 +0700

    add static file vendor css and js

--- tugasakhir/views.py
+++ tugasakhir/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
+
     return render(request, 'index.html')
+
+
+def store(request):
+
+    if(request.method == "POST"):
+        data = request.POST['data1']
+        print(data)
+
+        return render(request, 'index.html')  
